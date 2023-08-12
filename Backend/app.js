73e800dce41ced3cc7bb98df23111ee3194409fe
CharlesHuault <chuault@gmail.com>
2023-08-13 00:25:53 +0200

    Integration de la logique métier pour connexion et inscription

--- Backend/app.js
+++ Backend/app.js
@@ -11,8 +11,6 @@ mongoose.connect('mongodb+srv://charles:charles2023@cluster0.eqlilbc.mongodb.net
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use(express.json());
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -20,6 +18,8 @@ app.use((req, res, next) => {
   next();
 });
 
+
+app.use(express.json());
 app.use('/api/books', booksRoutes)
 app.use('/api/auth', userRoutes)
 
