349f51a512283a0710e2765bcc38d0bc10ad42ea
CharlesHuault <chuault@gmail.com>
2023-08-12 23:49:43 +0200

    Création du controller book et transfert des logiques métiers depuis le routeur

---
+++ Backend/controllers/book.js
@@ -0,0 +1,51 @@
+const Book = require('../models/book')
+
+exports.createBook = (req, res, next) => {
+    const book = new Book({
+        userId: req.body.userId,
+        title: req.body.title,
+        author: req.body.author,
+        imageUrl: req.body.imageUrl,
+        year: req.body.year,
+        genre: req.body.genre,
+        ratings: req.body.ratings,
+        averageRating: req.body.averageRating
+  });
+  book.save()
+    .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
+    .catch((error) => res.status(400).json({ error: error }));
+}
+
+exports.getOneBook = (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch((error) => res.status(404).json({ error: error }));
+}
+
+exports.modifyBook = (req, res, next) => {
+    const book = new Book({
+        userId: req.body.userId,
+        title: req.body.title,
+        author: req.body.author,
+        imageUrl: req.body.imageUrl,
+        year: req.body.year,
+        genre: req.body.genre,
+        ratings: req.body.ratings,
+        averageRating: req.body.averageRating
+  });
+    Book.updateOne({ _id: req.params.id }, book)
+    .then(() => res.status(200).json({ message: 'Livre modifié !'}))
+    .catch((error) => res.status(400).json({ error: error }));
+}
+
+exports.deleteBook = (req, res, next) => {
+    Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+}
+
+exports.getAllBooks = (req, res, next) => {
+        Book.find()
+        .then(book => res.status(200).json(book))
+        .catch((error) => res.status(400).json({ error: error }));
+}
