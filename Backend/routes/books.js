a18e74acfc43b2073f6ef9147b790929018bd022
CharlesHuault <chuault@gmail.com>
2023-08-23 10:16:23 +0200

    Intégration et configuration de sharp dans l'application

--- Backend/routes/books.js
+++ Backend/routes/books.js
@@ -5,14 +5,15 @@ const router = express.Router();
 const bookCtrl = require('../controllers/book')
 const auth = require('../middleware/auth')
 const multer = require('../middleware/multer-config')
+const sharp = require('../middleware/sharp')
 
 
 router.get('/', bookCtrl.getAllBooks);
 router.get('/bestrating', bookCtrl.getTopRatedBooks);
 router.get('/:id', bookCtrl.getOneBook);
-router.post('/', auth, multer, bookCtrl.createBook);
+router.post('/', auth, multer, sharp, bookCtrl.createBook);
 router.post('/:id/rating', auth, bookCtrl.rateBook)
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.put('/:id', auth, multer, sharp, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 
 module.exports = router;
