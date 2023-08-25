cbff2c0f5fe9a7687fad188a89e4e44424341922
CharlesHuault <chuault@gmail.com>
2023-08-25 09:58:49 +0200

    Version livrable du projet

--- Backend/controllers/book.js
+++ Backend/controllers/book.js
@@ -93,33 +93,33 @@ exports.rateBook = async (req, res, next) => {
         const userId = req.auth.userId;
         const rating = req.body.rating;
         const bookId = req.params.id;
-
+          // Vérifie si la note est valide (comprise entre 0 et 5)
         if (rating < 0 || rating > 5) {
             return res.status(400).json({ message: 'La note doit être comprise entre 0 et 5' });
         }
-
+        // Recherche le livre en utilisant son ID
         const book = await Book.findById(bookId);
 
         if (!book) {
             return res.status(404).json({ message: 'Livre non trouvé' });
         }
-
+        // Vérifie si l'utilisateur a déjà noté ce livre
         const userRatingIndex = book.ratings.findIndex(rating => rating.userId === userId);
 
         if (userRatingIndex !== -1) {
             return res.status(400).json({ message: 'Vous avez déjà noté ce livre' });
         }
-
+        // Ajoute la nouvelle note à la liste des notes du livre
         book.ratings.push({ userId, grade: rating });
-
+        // Calcule la nouvelle note moyenne en utilisant les notes existantes
         const totalRatings = book.ratings.length;
         const totalGrade = book.ratings.reduce((sum, rating) => sum + rating.grade, 0);
         const averageRating = totalGrade / totalRatings;
-
+        // Met à jour la note moyenne du livre
         book.averageRating = averageRating;
-
+        // Enregistre le livre mis à jour dans la base de données
         const savedBook = await book.save();
-
+        // Renvoie le livre mis à jour en réponse
         res.status(200).json(savedBook);
     } catch (error) {
         res.status(500).json({ error });
